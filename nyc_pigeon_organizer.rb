a300c566a23636ef7076120055ce0f2a8e570ec8
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-07-02 19:35:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |names|
+        if !pigeon_list.has_key? (names)
+          pigeon_list[names] = {}
+        end 
+        
+        if !pigeon_list[names].has_key? (property)
+          pigeon_list[names][property] = []
+        end 
+        
+        if !pigeon_list[names][property].include?(attribute)
+          pigeon_list[names][property].push(attribute.to_s)
+        end
+      end 
+    end 
+  end 
+  return pigeon_list
 end
